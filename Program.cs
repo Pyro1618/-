fcfd2d492a68909746f8268cee05904434ba04e7
Pyro1618 <45923976+Pyro1618@users.noreply.github.com>
2018-12-25 20:36:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			double x = 0;
+			double sum = 0;
+			double avg = 0;
+			int i = 1;
+			bool parsed;
+
+
+			for (avg = 0; avg <= 90; i++)
+			{
+				if (i > 1)
+				{
+					Console.WriteLine("");
+					Console.WriteLine("write another one");
+				}
+				else
+					Console.WriteLine("write a number");
+				parsed = double.TryParse(Console.ReadLine(), out x);
+
+
+				if (x <= 100 && parsed)
+				{
+					sum += x;
+					Console.WriteLine("");
+					Console.WriteLine("the Average is : " + (avg = sum / i));
+				}
+				else if ((i >= 1) || ((i >= 1) && (!parsed)))
+				{
+					Console.WriteLine("write something smaller or equal 100");
+					i -= 1;
+				}
+
+			}
+
+		}
+	}
+}
+
+		
